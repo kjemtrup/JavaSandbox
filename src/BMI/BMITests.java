2e84508dc475de24be08df78a82bf1bdbab8309c
kjemtrup <frederik.v.kjemtrup@gmail.com>
2018-03-18 18:05:22 +0100

    More TDD explored on BMI testing. Bully the people who help me because I'm just that kind of person. Also it's more fun that way.

--- src/BMI/BMITests.java
+++ src/BMI/BMITests.java
@@ -2,26 +2,59 @@ package BMI;
 
 import org.junit.jupiter.api.Test;
 
+import static java.lang.Math.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class BMITests {
 
     @Test
-    public void testPersonGettersConfirm{
+    public void testPersonFieldsConfirm() {
 
-        // Create person
+        Person Karsten = new Person("Karsten",180,90);
 
-        // Assert true that getters return expected vals
-
-
+        assertTrue(Karsten.name == "Karsten");
     }
 
     @Test
-    public void testPersonGettersDeny{
+    public void testPersonConstructorReturnsCorrectly() {
 
-        // Create person
+        Person Benjamin = new Person("Benjamin", 200, 60);
 
-        // Assert false that getters return UNexpected vals
+        assertTrue(Benjamin.name == "Benjamin");
+        assertTrue(Benjamin.height == 200);
+    }
 
+    @Test
+    public void testPersonBMICalc(){
 
+        Person Yaro = new Person("Yaroslav",1.82,130);
+
+        assertTrue(Yaro.getBMI() == 130 / pow(1.82,2));
+    }
+
+    @Test
+    public void testPersonBMICalcAccurate(){
+
+        Person Yaro = new Person("Yaroslav",1.82,130);
+
+        assertFalse(Yaro.getBMI() == 131 / pow(1.82,2));
+    }
+
+    @Test
+    public void testBMIYaroObese(){
+
+        Person Yaro = new Person("Yaroslav",1.82,130);
+
+        assertTrue(Yaro.BMIClassification() == "Fat fuck");
+    }
+
+    @Test
+    public void testBMIKurtTwink(){
+
+        Person Kurt = new Person("Kurt", 1.50, 55);
+
+        assertTrue(Kurt.BMIClassification() == "Twinklet spotted");
     }
 
 }
--- src/BMI/Person.java
+++ src/BMI/Person.java
@@ -2,5 +2,32 @@ package BMI;
 
 public class Person {
 
-    // Mange fields
+    String name;
+    double height;
+    double weight;
+
+    public Person(String name, double height, double weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public double getBMI() {
+        double BMI = this.weight / Math.pow(this.height, 2);
+        return BMI;
+    }
+
+    public String BMIClassification(){
+        if (this.getBMI() > 30){
+            return "Fat fuck";
+        }
+        else if (this.getBMI() < 30 && this.getBMI() > 25){
+            return "Thick, solid, tight";
+        }
+        else if (this.getBMI() < 25){
+            return "Twinklet spotted";
+        }
+        else return "What are you even?";
+    }
+
 }
